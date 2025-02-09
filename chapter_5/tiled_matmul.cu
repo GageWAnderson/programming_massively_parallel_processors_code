f52f14ea0227e47f1b6660b33c1b5fc46a70d64c
GageWAnderson <gage.anderson@bcg.com>
2025-02-08 20:31:36 -0500

    feat: added boundary checked kernel

--- chapter_5/tiled_matmul.cu
+++ chapter_5/tiled_matmul.cu
@@ -38,6 +38,48 @@ __global__ void matrixMulKernel(float *M, float *N, float *P, int Width)
     P[Row * Width + Col] = PValue;
 }
 
+__global__ void matrixMulKernelBoundaryChecked(float *M, float *N, float *P, int Width)
+{
+    // Kernel does the TILE_SIZE (nice) part then the boundary part
+    
+    // Tile input matricies into shared memory so multiple threads within a block can use
+    __shared__ float Mds[TILE_SIZE][TILE_SIZE];
+    __shared__ float Nds[TILE_SIZE][TILE_SIZE];
+
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    // Identify the row, col of the element to work on
+    int ROW = by * TILE_SIZE + ty;
+    int Col = bx * TILE_SIZE + tx;
+
+    // Loop over the M and N tiles required to compute P
+    float Pvalue = 0;
+    for (int ph = 0; ph < Width / TILE_SIZE; ++ph)
+    {
+        // Collaborative loading of M and N tiles into shared memory
+        if ((Row < Width) && (ph * TILE_SIZE + tx) < Width)
+            Mds[ty][tx] = M[Row * Width + ph * TILE_SIZE + tx];
+        else
+            Mds[ty][tx] = 0.0f;
+        if ((ph * TILE_SIZE + ty) < Width && Col < Width)
+            Nds[ty][tx] = N[(ph * TILE_SIZE + ty) * Width + Col];
+        else
+            Nds[ty][tx] = 0.0f;
+        __syncthreads();
+
+        for (int k = 0; k < TILE_SIZE; ++k)
+        {
+            Pvalue += Mds[ty][k] * Nds[k][tx];
+        }
+        __syncthreads();
+    }
+    if ((Row < Width) && (Col < Width))
+        P[Row * Width + Col] = PValue;
+}
+
 int main(int argc, char **argv)
 {
     // Initialize the input matrices
@@ -68,7 +110,7 @@ int main(int argc, char **argv)
     cudaMemcpy(d_N, h_N, Width * Height * sizeof(float), cudaMemcpyHostToDevice);
 
     // Launch the kernel on the device
-    matrixMulKernel<<<dim3(Width / TILE_SIZE, Height / TILE_SIZE), dim3(TILE_SIZE, TILE_SIZE)>>>(d_M, d_N, d_P, Width);
+    matrixMulKernelBoundaryChecked<<<dim3(Width / TILE_SIZE, Height / TILE_SIZE), dim3(TILE_SIZE, TILE_SIZE)>>>(d_M, d_N, d_P, Width);
 
     // Copy the output matrix from the device to the host
     cudaMemcpy(h_P, d_P, Width * Height * sizeof(float), cudaMemcpyDeviceToHost);
